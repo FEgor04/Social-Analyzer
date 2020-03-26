42aaadcc1ddfb8f2f2ef756d1fdba5c7776524b6
Egor Fedorov <fegor2004@gmail.com>
2020-03-26 19:42:43 +0300

    Tests with GitHub Workflows

--- __main__.py
+++ __main__.py
@@ -1,8 +1,11 @@
-import vk_scrapper
+import matplotlib.pyplot as plt
+import networkx as nx
+
 import social_graph
 
 if __name__ == '__main__':
-    all_friends = social_graph.get_all_friends("fegor2004", 2)
-    print(len(all_friends))
-    for person in all_friends:
-        print(person)
+    fig = plt.figure(figsize=(24, 24))
+    target = "fegor2004"
+    cnt = 1
+    graph_nx = social_graph.get_friends_nx_graph(target, cnt)
+    nx.write_adjlist(graph_nx, f"{target}_friends_graph_{cnt}_1.adjlist")
